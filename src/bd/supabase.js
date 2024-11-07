77d3494c1ce79ba5e2019452a980ed330eb2613d
Pauibarr <paulonesyt16@gmail.com>
2024-11-07 13:01:54 +0100

    segundo

--- src/bd/supabase.js
+++ src/bd/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://gpbcntersjgydvltolrr.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwYmNudGVyc2pneWR2bHRvbHJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkxNTc4MzIsImV4cCI6MjA0NDczMzgzMn0.NigsnwML3xN3ukkit1y7kMNJsrsUBd6IOOt9HzZ7pVc'
-const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
 
 const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwYmNudGVyc2pneWR2bHRvbHJyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTE1NzgzMiwiZXhwIjoyMDQ0NzMzODMyfQ.6-mN5SPLiF44tZPtq_uGMqv6WB-BXElCRbH_92QCQ5w'
-const supabaseService = createClient(supabaseUrl, supabaseServiceKey)
+export const supabaseService = createClient(supabaseUrl, supabaseServiceKey)
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,9 @@
-import { Link } from "react-router-dom";
 
 export const Footer = () => {
     return (
         <footer className="bg-gray-900 text-white p-4 fixed bottom-0 left-0 right-0 py-4">
           <div className="container mx-auto flex justify-between items-center">
             <p className="text-sm">&copy; {new Date().getFullYear()} ShoeStore. All rights reserved.</p>
-            <ul className="flex space-x-4">
-              <li>
-                <a href="/privacy" className="hover:text-gray-400">
-                  Privacy Policys
-                </a>
-              </li>
-              <li>
-                <a href="/terms" className="hover:text-gray-400">
-                  Terms of Services
-                </a>
-              </li>
-            </ul>
           </div>
         </footer>
       );
